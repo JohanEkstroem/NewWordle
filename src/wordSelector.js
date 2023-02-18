0dfbc9b11a2d4f3dca6da608fd5ed32535c6ff62
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2023-02-18 12:34:07 +0100

    Add unique word if required

--- src/wordSelector.js
+++ src/wordSelector.js
@@ -1,2 +1,16 @@
-const wordSelector = () => 'WORD';
+const wordSelector = (listOfWords, length, isUnique) => {
+  let listOfWordsCorrectLength = listOfWords.filter((e) => e.length == length);
+
+  const uniqueChars = listOfWordsCorrectLength.filter(
+    (e) => [...new Set(e.split(''))].join('') == e
+  );
+
+  if (isUnique === true) {
+    return uniqueChars[Math.floor(Math.random() * uniqueChars.length)];
+  }
+
+  return listOfWordsCorrectLength[
+    Math.floor(Math.random() * listOfWordsCorrectLength.length)
+  ];
+};
 export default wordSelector;
--- src/wordSelector.test.js
+++ src/wordSelector.test.js
@@ -1,10 +1,18 @@
 import { expect, jest, test } from '@jest/globals';
 import wordSelector from './wordSelector';
 
-test('Given a list of words then returns a string', () => {
-  const input = [];
-  const result = 'WORD';
-  const output = wordSelector(input);
+test('Given a list and a int(7) should return "SABATON"', () => {
+  const input = ['SNÖ', 'APAN', 'KAFFE', 'SABATON'];
+  const result = 'SABATON';
+  const output = wordSelector(input, 7, false);
+
+  expect(output).toEqual(result);
+});
+
+test('Given a list, an int(4) and isUnique true should return "EPAN"', () => {
+  const input = ['SNÖ', 'APAN', 'EPAN', 'KAFFE', 'SABATON'];
+  const result = 'EPAN';
+  const output = wordSelector(input, 4, true);
 
   expect(output).toEqual(result);
 });
